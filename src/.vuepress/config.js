357638858e9c9b7801496c14151e531195e8d806
Nakatai-0322 <nakatai0322@gmail.com>
2022-02-05 07:24:41 +0900

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -10,6 +10,9 @@ module.exports = {
     head: [
         ["link", { rel: "icon", href: "//minotar.net/helm/d297f52003024e5d944fd78edc82891a"}],
         ["link", { href: "/css/style.css", rel: "stylesheet"}],
+        ["meta", { charset: "UTF-8"}],
+        ["meta", { "http-equiv": "X-UA-Compatible", content: "IE=edge"}],
+        ["meta", { name: "viewport", content: "width=device-width, initial-scale=1.0"}]
     ],
     locales: {
         "/": {
