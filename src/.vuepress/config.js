8dfe820c1034b337bf42ce75db4aa0dd3d0fc858
Nakatai <nakatai0322@gmail.com>
2022-05-14 10:51:54 +0900

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -13,7 +13,6 @@ module.exports = {
         ["meta", { charset: "UTF-8" }],
         ["meta", { "http-equiv": "X-UA-Compatible", content: "IE=edge" }],
         ["meta", { name: "viewport", content: "width=device-width, initial-scale=1.0" }],
-        ["script", { src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2041303981654233", async: true,  crossorigin: "anonymous"}],
     ],
     locales: {
         "/": {
