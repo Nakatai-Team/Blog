54a91f5b16b2c9ca4bdec54b1419234662d607b5
Nakatai <nakatai0322@gmail.com>
2022-06-18 23:29:37 +0900

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -89,8 +89,8 @@ module.exports = {
                 <br>
                 &copy; 2021-present Nakatai Some Right Reserved.
                 <br>
-                <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
-                    <img alt="クリエイティブ・コモンズ・ライセンス" style="border-width:0" width="120" height="42" src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-sa.svg">
+                <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+                    <img alt="クリエイティブ・コモンズ・ライセンス" style="border-width:0" width="120" height="42" src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by.svg">
                 </a>
                 <br>
                 <a xmlns:cc="http://creativecommons.org/ns#" href="https://nakatai.ga" property="cc:attributionName" rel="cc:attributionURL">
@@ -99,8 +99,8 @@ module.exports = {
                 <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">
                     Nakatai Blog
                 </span>"は
-                <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
-                    クリエイティブ・コモンズ 表示 - 継承 4.0 国際 ライセンス
+                <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+                    クリエイティブ・コモンズ 表示 4.0 国際 ライセンス
                 </a>で提供されています。
                 <br>
                 `,
