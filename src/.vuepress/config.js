451a7222ce570e81b4e026203581997cb3629445
Nakatai-0322 <nakatai0322@gmail.com>
2022-06-29 18:41:44 +0900

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -26,7 +26,7 @@ module.exports = {
         "sitemap": {
             hostname: "https://nakatai.ga",
             exclude: ["/404"],
-            changefreq: "weekly",
+            changefreq: "daily",
             dateFormatter: val => {
                 return new Date().toISOString()
             },
